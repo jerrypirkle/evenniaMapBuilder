08f619c45f9d5468ff953ce07c397e185f692333
Jerry Pirkle <36513655+jerrypirkle@users.noreply.github.com>
2020-04-10 13:40:20 -0500

    Add files via upload

---
+++ locget.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+Read a csv 'map' and pull all locations
+"""
+
+__author__ = "╰(｡•́‿•̀｡)つ──★*:・ﾟ"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+# Import modules
+import sys
+import pandas as pd
+from datetime import datetime
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+import configparser
+configF = 'config.ini'
+parser = configparser.ConfigParser()
+parser.read(configF)
+
+# Set global variables
+# set data file
+parser.get('conf', 'data')
+# set zone
+parser.get('conf', 'zone')
+dt = datetime.now()
+desctimestamp = dt.strftime("%m/%d/%y - %H:%M:%S")
+descriptions = {}
+
+
+def main():
+    """
+    > Main entry point of the app
+    Get the dimensions of the dataframe and assign them to variables
+    Then, write variables to config
+    """
+    x,y = list(range(data.shape[0])), list(range(data.shape[1]))
+
+    # Get all possible coordinates
+    cords = [(l,r) for l in x for r in y]
+
+    # Hit every item in the dataframe
+    for p in cords:
+        location = (p[0],p[1])
+        #p is a coordiante tuple (x,y) from the list of all coordinates
+        room = data.iloc[location]
+        x,y = location                            # assign location coordinates to variables
+        if room != 'empty':
+            descriptions[f"{zone} - {room}"] = ""
+
+    pp.pprint(descriptions)     
+    parser.set('desc', 'descriptions', str(descriptions))
+    parser.set('desc', 'desctimestamp', str(desctimestamp))
+    with open(configF, "w+") as configfile:
+        parser.write(configfile)
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
